012c6db2c87d81f4d86f9264f31d4f653e1bb653
Lucas Inocente <olucassanchez@gmail.com>
2019-10-03 00:24:46 -0300

    Add wip login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Messages from './containers/Messages/Messages';
+import Login from './containers/Login/Login';
 import './App.css';
 
 function App() {
   return (
     <>
+      <Login />
       <Messages />
     </>
   );
---
+++ src/containers/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+  return (
+    <div className="App">
+      <section className="container row">
+        Login
+      </section>
+    </div>
+  );
+}
+
+export default Login;
