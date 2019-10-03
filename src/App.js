ea1fe74de4dd45b6e7c641796a0b62cbc195c8a0
Lucas Inocente <olucassanchez@gmail.com>
2019-10-03 00:39:28 -0300

    Add routes and add wip for public chat component
    This will finish all containers we need for MVP.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Messages from './containers/Messages/Messages';
 import Login from './containers/Login/Login';
+import Chat from './containers/Chat/Chat';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Login />
-      <Messages />
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/messages" exact={true} component={Messages}/>
+        <Route path="/login" exact={true} component={Login}/>
+        <Route path="/chat" exact={true} component={Chat}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/containers/Chat/Chat.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Chat.css';
+
+const Chat = () => {
+  return (
+    <div className="App">
+      <section className="container row">
+        Chat
+      </section>
+    </div>
+  );
+}
+
+export default Chat;
