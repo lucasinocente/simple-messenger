0cbeeafc55468c534dfd6042ef00c5f99ee52b7a
Lucas Inocente <olucassanchez@gmail.com>
2019-10-13 11:46:58 -0300

    WIP: Read and send messages

--- src/containers/Messages/Messages.js
+++ src/containers/Messages/Messages.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './Messages.css';
-
+import React, { useEffect, useState  } from 'react';
 import firebase from '../../firebase/Firebase';
 
+import './Messages.css';
+
+const database = firebase.firestore();
+
 const logout = async () => {
-  console.log('funcionou');
   try {
     await firebase.auth().signOut();
     window.location.href = '/login';
@@ -13,27 +14,102 @@ const logout = async () => {
   }
 };
 
+const addUser = async (user, database) => {
+  const returns = await database.collection("users").doc(user.uid).set({
+    uid: user.uid
+  });
+  console.log(returns);
+};
+
+const sendMessage = async (event, uid, message) => {
+  event.preventDefault();
+  const returns = await database.collection("messages").add({
+    room: uid,
+    uid,
+    message,
+    timestamp: Date.now()
+  });
+  console.log(returns);
+};
+
+const getUser = async (uid) => {
+  const user = await database.collection("users").doc(uid);
+
+  user.get().then(async function(user) {
+    if (user.exists) {
+      console.log("Document data:", user.data());
+    } else {
+      await addUser(user, database);
+    }
+    return;
+  }).catch(function(error) {
+      console.log("Error getting document:", error);
+  });
+};
+
+const getMessages = async (uid, setConversation) => {
+  try {
+    const messages = database.collection("messages").where("room", "==", uid);
+
+    messages.get().then(function(snapshot) {
+
+    //  const messages = [];
+      snapshot.forEach(function(snap) {
+        console.log(snap.id, " => ", snap.data());
+        const { message, uid } = snap.data();
+      });
+
+     // setConversation(messages);
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+  } catch (error) {
+    console.log("Error cathing document:", error);
+  }
+
+};
+
 const Messages = () => {
+  const [conversation, setConversation] = useState();
+  const [message, setMessage] = useState();
+  const [uid, setUid] = useState();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(async function(user) {
+      if (user) {
+        const { uid } = user;
+        setUid(uid);
+        getUser(uid);
+        getMessages(uid, setConversation);
+      } else {
+        window.location.href = '/login';
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header>
-        <button type="button" onClick={logout}>Logout</button>
-      </header>
-      <section className="container row">
-        <div className="column column-messages">
-          <div className="messages">
-            <div className="message message-received">
-              Olá!
+      <section className="container-messages">
+        <header>
+          <button type="button" onClick={logout}>Logout</button>
+        </header>
+        <div className="container row ">
+          <div className="column column-messages">
+            <div className="messages">
+              <div className="message message-received">
+                Olá!
+              </div>
+              <div className="message message-send">
+                Oi, como vai você?
+              </div>
             </div>
-            <div className="message message-send">
-              Oi, como vai você?
+            { conversation }
+            <div className="form">
+              <form>
+                <input type="text" onChange={(e) => setMessage(e.target.value)}></input>
+                <button type="button" onClick={(e) => sendMessage(e, uid, message)}>Send</button>
+              </form>
             </div>
-          </div>
-          <div className="form">
-            <form>
-              <input type="text"></input>
-              <button type="button">Send</button>
-            </form>
           </div>
         </div>
       </section>
--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,4 +1,5 @@
 import firebase from "firebase/app";
+import 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
