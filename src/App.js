db0755a5ca07df41b6755eb184ecd7729152ed0c
Lucas Inocente <olucassanchez@gmail.com>
2019-10-13 12:25:02 -0300

    Fix order and update files for routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Messages from './containers/Messages/Messages';
+import MessagesList from './containers/Messages/MessagesList';
 import Login from './containers/Login/Login';
 import Verify from './containers/Login/Verify';
-import Chat from './containers/Chat/Chat';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -12,11 +12,11 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/messages/:uid" exact={true} component={Messages}/>
         <Route path="/" exact={true} component={Login}/>
         <Route path="/login" exact={true} component={Login}/>
         <Route path="/verify" exact={true} component={Verify}/>
-        <Route path="/chat" exact={true} component={Chat}/>
+        <Route path="/messages" exact={true} component={MessagesList}/>
+        <Route path="/messages/:uid" exact={true} component={Messages}/>
       </Switch>
     </BrowserRouter>
   );
